200d4a32c88893b23171be268dedb16f80555d29
yosiassapari <yosiassapari@gmail.com>
2022-04-18 12:44:04 -0700

    button widget

---
+++ lib/my_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String? child;
+  final Color? color;
+  final Color? textColor;
+  final function;
+
+  MyButton({
+    required this.child,
+    required this.color,
+    required this.textColor,
+    required this.function,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                child!,
+                style: TextStyle(
+                  color: textColor,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
