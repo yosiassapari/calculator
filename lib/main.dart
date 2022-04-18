a17361bf33cd5ff938f60dd8e63e386e90dd12ab
yosiassapari <yosiassapari@gmail.com>
2022-04-18 12:43:52 -0700

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
+    );
+  }
+}
